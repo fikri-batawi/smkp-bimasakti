a2e926bda77799d341f87b25445ecee1989ff4a4
Fikri <fikri2347@gmail.com>
2020-05-15 22:27:02 +0700

    Fix Base URL

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Web-SMKP/';
+// $config['base_url'] = 'http://localhost/Web-SMKP/';
+$config['base_url'] = 'http://smkp.kulon-ti.com/';
 
 /*
 |--------------------------------------------------------------------------
